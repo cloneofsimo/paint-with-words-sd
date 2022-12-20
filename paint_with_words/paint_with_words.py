2074123e91c12ef35983b0f2c9e4647066d9dae1
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2022-12-20 12:19:46 +0900

    port `inj_forward` to `attention.py`

--- paint_with_words/paint_with_words.py
+++ paint_with_words/paint_with_words.py
@@ -42,63 +42,6 @@ def _pil_from_latents(vae, latents):
     return ret_pil_images
 
 
-def inj_forward(self, hidden_states, context=None, mask=None):
-
-    is_dict_format = True
-    if context is not None:
-        try:
-            context_tensor = context["CONTEXT_TENSOR"]
-        except:
-            context_tensor = context
-            is_dict_format = False
-
-    else:
-        context_tensor = hidden_states
-
-    batch_size, sequence_length, _ = hidden_states.shape
-
-    query = self.to_q(hidden_states)
-
-    key = self.to_k(context_tensor)
-    value = self.to_v(context_tensor)
-
-    dim = query.shape[-1]
-
-    query = self.reshape_heads_to_batch_dim(query)
-    key = self.reshape_heads_to_batch_dim(key)
-    value = self.reshape_heads_to_batch_dim(value)
-
-    attention_scores = torch.matmul(query, key.transpose(-1, -2))
-
-    attention_size_of_img = attention_scores.shape[-2]
-    if context is not None:
-        if is_dict_format:
-            f: Callable = context["WEIGHT_FUNCTION"]
-            w = context[f"CROSS_ATTENTION_WEIGHT_{attention_size_of_img}"]
-            sigma = context["SIGMA"]
-
-            cross_attention_weight = f(w, sigma, attention_scores)
-        else:
-            cross_attention_weight = 0.0
-    else:
-        cross_attention_weight = 0.0
-
-    attention_scores = (attention_scores + cross_attention_weight) * self.scale
-
-    attention_probs = attention_scores.softmax(dim=-1)
-
-    hidden_states = torch.matmul(attention_probs, value)
-
-    hidden_states = self.reshape_batch_dim_to_heads(hidden_states)
-
-    # linear proj
-    hidden_states = self.to_out[0](hidden_states)
-    # dropout
-    hidden_states = self.to_out[1](hidden_states)
-
-    return hidden_states
-
-
 def pww_load_tools(
     device: str = "cuda:0",
     scheduler_type=LMSDiscreteScheduler,
