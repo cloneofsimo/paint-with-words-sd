37ab9fdc34332696ac8e9fbdf3e8c5e3422b3b91
Li-Wei <j6309355065@gmail.com>
2023-03-07 13:11:08 +0800

    updating ui for pww inpainting

--- gradio_pww.py
+++ gradio_pww.py
@@ -99,66 +99,59 @@ def collect_color_content(*args):
         return ""
 
 
-
-# Create a black image with shape (100, 20)
-data = np.zeros((100, 5), dtype=np.uint8)
-blank_image = Image.fromarray(data)
-# Make the image read-only
-blank_image.load = lambda: None
-blank_image.readonly = True
-
-block = gr.Blocks().queue()
-with block:
-    with gr.Row():
-        gr.Markdown("## Paint-with-word")
-    with gr.Row():
-        with gr.Column():
-            with gr.Row():
-                color_map_image = gr.Image(label='Segmentation map', source='upload', type='pil', tool='color-sketch', id='segmentation-map')
-                init_image = gr.Image(label='Initial image', source='upload', type='pil')
-            prompt = gr.Textbox(label="Prompt")
-            color_context = gr.Textbox(label="Color context", value='')
-            run_button = gr.Button(value="Run Paint-with-Word")            
-
-        with gr.Column():
-            with gr.Accordion("Color content options", open=False):
+if __name__ == '__main__':
+    block = gr.Blocks().queue()
+    with block:
+        with gr.Row():
+            gr.Markdown("## Paint-with-word")
+        with gr.Row():
+            with gr.Column():
                 with gr.Row():
-                    extract_color_boxes_button = gr.Button(value="Extract color content")
-                    generate_color_boxes_button = gr.Button(value="Generate color content")
-                prompts = []
-                strengths = []
-                seeds = []
-                color_maps = []
-                colors = [gr.Textbox(value="", visible=False) for i in range(MAX_NUM_COLORS)]
-                for n in range(MAX_NUM_COLORS):
-                    with gr.Accordion('item%d'%n):
-                        with gr.Row():
-                            color_maps.append(gr.Image(image=create_canvas(15,3), interactive=False, type='numpy'))
-                            with gr.Column():
-                                prompts.append(gr.Textbox(label="Prompt", interactive=True))
-                                with gr.Row():
-                                    strengths.append(gr.Textbox(label="Strength", interactive=True))
-                                    seeds.append(gr.Textbox(label="Random Seed", interactive=True))
-                        
-            with gr.Accordion("Advanced options", open=False):
-                width = gr.Slider(label="Width", minimum=256, maximum=1024, value=512, step=256)
-                height = gr.Slider(label="Height", minimum=256, maximum=1024, value=512, step=256)
-                num_samples = gr.Slider(label="Images", minimum=1, maximum=12, value=1, step=1)
-                ddim_steps = gr.Slider(label="Steps", minimum=1, maximum=100, value=30, step=1)
-                scale = gr.Slider(label="Guidance Scale", minimum=0.1, maximum=30.0, value=7.5, step=0.1)
-                seed = gr.Slider(label="Seed", minimum=-1, maximum=2147483647, step=1, value=0)
-                eta = gr.Number(label="eta (DDIM)", value=0.5)
-                a_prompt = gr.Textbox(label="Added Prompt", value='')
-                n_prompt = gr.Textbox(label="Negative Prompt", value='')
-            device = gr.inputs.Dropdown(label='Device', default='cuda', choices=['cuda', 'mps'])
+                    color_map_image = gr.Image(label='Segmentation map', source='upload', type='pil', tool='color-sketch', id='segmentation-map')
+                    init_image = gr.Image(label='Initial image', source='upload', type='pil')
+                prompt = gr.Textbox(label="Prompt")
+                color_context = gr.Textbox(label="Color context", value='')
+                run_button = gr.Button(value="Run Paint-with-Word")            
 
-            with gr.Row():
-                gr.Markdown("### Results")
-            result_gallery = gr.Gallery(label='Output', show_label=False, elem_id="gallery").style(grid=2, height='auto')
-    extract_color_boxes_button.click(fn=extract_color_textboxes, inputs=[color_map_image], outputs=[*color_maps, *prompts, *strengths, *seeds, *colors])
-    generate_color_boxes_button.click(fn=collect_color_content, inputs=[*colors, *prompts, *strengths, *seeds], outputs=[color_context])
+            with gr.Column():
+                with gr.Accordion("Color content options", open=False):
+                    with gr.Row():
+                        extract_color_boxes_button = gr.Button(value="Extract color content")
+                        generate_color_boxes_button = gr.Button(value="Generate color content")
+                    prompts = []
+                    strengths = []
+                    seeds = []
+                    color_maps = []
+                    colors = [gr.Textbox(value="", visible=False) for i in range(MAX_NUM_COLORS)]
+                    for n in range(MAX_NUM_COLORS):
+                        with gr.Accordion('item%d'%n):
+                            with gr.Row():
+                                color_maps.append(gr.Image(image=create_canvas(15,3), interactive=False, type='numpy'))
+                                with gr.Column():
+                                    prompts.append(gr.Textbox(label="Prompt", interactive=True))
+                                    with gr.Row():
+                                        strengths.append(gr.Textbox(label="Strength", interactive=True))
+                                        seeds.append(gr.Textbox(label="Random Seed", interactive=True))
+                            
+                with gr.Accordion("Advanced options", open=False):
+                    width = gr.Slider(label="Width", minimum=256, maximum=1024, value=512, step=256)
+                    height = gr.Slider(label="Height", minimum=256, maximum=1024, value=512, step=256)
+                    num_samples = gr.Slider(label="Images", minimum=1, maximum=12, value=1, step=1)
+                    ddim_steps = gr.Slider(label="Steps", minimum=1, maximum=100, value=30, step=1)
+                    scale = gr.Slider(label="Guidance Scale", minimum=0.1, maximum=30.0, value=7.5, step=0.1)
+                    seed = gr.Slider(label="Seed", minimum=-1, maximum=2147483647, step=1, value=0)
+                    eta = gr.Number(label="eta (DDIM)", value=0.5)
+                    a_prompt = gr.Textbox(label="Added Prompt", value='')
+                    n_prompt = gr.Textbox(label="Negative Prompt", value='')
+                device = gr.inputs.Dropdown(label='Device', default='cuda', choices=['cuda', 'mps'])
 
-    ips = [color_map_image, init_image, color_context, prompt, a_prompt, n_prompt, num_samples, ddim_steps, scale, seed, eta, device, width, height]
-    run_button.click(fn=run_pww, inputs=ips, outputs=[result_gallery])   
-    
-block.launch(server_name='0.0.0.0')
+                with gr.Row():
+                    gr.Markdown("### Results")
+                result_gallery = gr.Gallery(label='Output', show_label=False, elem_id="gallery").style(grid=2, height='auto')
+        extract_color_boxes_button.click(fn=extract_color_textboxes, inputs=[color_map_image], outputs=[*color_maps, *prompts, *strengths, *seeds, *colors])
+        generate_color_boxes_button.click(fn=collect_color_content, inputs=[*colors, *prompts, *strengths, *seeds], outputs=[color_context])
+
+        ips = [color_map_image, init_image, color_context, prompt, a_prompt, n_prompt, num_samples, ddim_steps, scale, seed, eta, device, width, height]
+        run_button.click(fn=run_pww, inputs=ips, outputs=[result_gallery])   
+        
+    block.launch(server_name='0.0.0.0')
